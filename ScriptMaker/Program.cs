3eec51c2fd188059fd2f26c77c329762ddabe8fb
ajvavjuhani666 <53824157+ajvavjuhani666@users.noreply.github.com>
2019-10-12 20:24:49 +0200

    test

---
+++ ScriptMaker/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using Script_Maker.Champions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Script_Maker
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += On_LoadGame;
+        }
+        private static void On_LoadGame()
+        {
+           
+                ScriptMaker.OnLoad();
+                Chat.Print("Welcome to Script Maker :)");
+            
+        }
+    }
+}
